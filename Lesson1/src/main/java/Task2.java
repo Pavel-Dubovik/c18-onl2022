f1f0af7f44c8b3a0d840510101ceebcac015e23a
Dubovik Pavel <dubovik.pg@gmail.com>
2022-10-21 18:15:47 +0300

    Task2. Исправил замечания.

--- Lesson1/src/main/java/Task2.java
+++ Lesson1/src/main/java/Task2.java
@@ -5,17 +5,15 @@ public class Task2 {
         Scanner console = new Scanner(System.in);
         System.out.println("Введите количество значений:");
         int n = console.nextInt();
-        Scanner scanner = new Scanner(System.in);
-        String[] array = new String[n];
         int count = 0;
         for (int i = 0; i < n; i++) {
             System.out.println("Введите " + (i + 1) + "-е число:");
-            array[i] = scanner.nextLine();
-            int val = Integer.parseInt(array[i]);
+            String number = console.next();
+            int val = Integer.parseInt(number);
             if (val > 0) {
                 count = count + 1;
             }
         }
-        System.out.println("Количесттво положительных чисел = " + count);
+        System.out.println("Количество положительных чисел = " + count);
     }
 }
