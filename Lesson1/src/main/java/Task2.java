21f807070450323e23be0ea0ad3ac5a5bd9910d8
Dubovik Pavel <dubovik.pg@gmail.com>
2022-10-23 13:10:55 +0300

    Правильный вариант решения задачи. Не знал, что при запуске программы через командную строку можно ввести данные и они попадут в качестве аргумента в метод main.))

--- Lesson1/src/main/java/Task2.java
+++ Lesson1/src/main/java/Task2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
+        /*Scanner console = new Scanner(System.in);
         System.out.println("Введите количество значений:");
         int n = console.nextInt();
         int count = 0;
@@ -14,6 +14,15 @@ public class Task2 {
                 count = count + 1;
             }
         }
-        System.out.println("Количество положительных чисел = " + count);
+        System.out.println("Количество положительных чисел = " + count);*/
+
+        int count = 0;
+        for (String arg : args) {
+            int value = Integer.parseInt(arg);
+            if (value > 0) {
+                count = count + 1;
+            }
+        }
+        System.out.println(count);
     }
 }
