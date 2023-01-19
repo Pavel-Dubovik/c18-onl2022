9c69d967115823679177e1009d0bd7d75fefeb09
Dubovik Pavel <dubovik.pg@gmail.com>
2023-01-19 22:54:32 +0300

    Added homework 21. An additional task.

--- Lesson21/src/main/java/hw21/model/Student.java
+++ Lesson21/src/main/java/hw21/model/Student.java
@@ -14,6 +14,7 @@ public class Student {
     private String name;
     private String surname;
     private String course;
+    private String city;
 
     public Student(String name, String surname, String course) {
         this.name = name;
@@ -21,4 +22,10 @@ public class Student {
         this.course = course;
     }
 
+    public Student(int id, String name, String surname, String course) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.course = course;
+    }
 }
--- Lesson21/src/main/java/hw21/utils/CRUDUtils.java
+++ Lesson21/src/main/java/hw21/utils/CRUDUtils.java
@@ -1,16 +1,20 @@
 package hw21.utils;
 
 import hw21.model.Student;
+import lombok.experimental.UtilityClass;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@UtilityClass
 public class CRUDUtils {
     public static final String INSERT_STUDENT_QUERY = "insert into students(name, surname, course) VALUES(?, ?, ?)";
     public static final String SELECT_ALL_STUDENTS_QUERY = "select * from students";
     public static final String UPDATE_STUDENT_QUERY = "update students set course = ? where id = ?";
     public static final String DELETE_STUDENT_QUERY = "delete from students where id = ?";
+    public static final String INSERT_CITY_QUERY = "insert into cities(student_id, city) values(?, ?)";
+    public static final String SELECT_ALL_STUDENTS_WITH_CITY_QUERY = "select * from students, cities where cities.student_id = students.id";
 
 
     public static List<Student> getAllStudents() {
@@ -78,5 +82,39 @@ public class CRUDUtils {
             System.out.println(e.getMessage());
         }
         return updateStudents;
+    }
+
+    public static void saveCity(String city, int studentId) {
+
+        try (Connection connection = DbUtils.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_CITY_QUERY);
+            preparedStatement.setInt(1, studentId);
+            preparedStatement.setString(2, city);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static List<Student> getAllStudentWithCities() {
+        List<Student> studentsWithCities = new ArrayList<>();
+
+        try (Connection connection = DbUtils.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(SELECT_ALL_STUDENTS_WITH_CITY_QUERY);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String surname = resultSet.getString("surname");
+                String course = resultSet.getString("course");
+                String city = resultSet.getString("city");
+                studentsWithCities.add(new Student(id, name, surname, course, city));
+
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return studentsWithCities;
     }
 }
