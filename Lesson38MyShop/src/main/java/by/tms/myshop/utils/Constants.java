4a0e74a974bedffeb4bfc83c7a0ba21589caa637
Dubovik Pavel <dubovik.pg@gmail.com>
2023-04-16 18:06:56 +0300

    Added MyShop.

--- Lesson38MyShop/src/main/java/by/tms/myshop/utils/Constants.java
+++ Lesson38MyShop/src/main/java/by/tms/myshop/utils/Constants.java
@@ -5,28 +5,10 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Constants {
 
-    /**
-     * Commands
-     */
     public static final String HOME_COMMAND = "home";
     public static final String CATEGORY_COMMAND = "category";
     public static final String PRODUCT_COMMAND = "product";
     public static final String CART_COMMAND_GET = "cart-get";
     public static final String CART_COMMAND_POST = "cart-post";
-    /**
-     * PagePath
-     */
-   /* public static final String SIGN_IN_PAGE_PATH = "signin.jsp";
-    public static final String HOME_PAGE_PATH = "home.jsp";
-    public static final String CATEGORY_PAGE_PATH = "category.jsp";
-    public static final String PRODUCT_PAGE_PATH = "product.jsp";
-    public static final String PRODUCT_CONTROLLER_PATH = "/myshop?command=product&productId=product.getId()";
-    public static final String CART_PAGE_PATH = "cart.jsp";
-    public static final String CART_CONTROLLER_PATH = "/myshop?command=cart-get";*/
-    /**
-     * RequestParam
-     */
-   /* public static final String LOGIN = "username";
-    public static final String PASSWORD = "password";*/
     public static final String COMMAND = "command";
 }
